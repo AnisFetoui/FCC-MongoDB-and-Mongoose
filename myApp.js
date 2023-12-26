112ff35f124db5ec32d2b758a0c37942dc8faa98
Anis FETOUI <anis.fetoui@esprit.tn>
2023-12-26 02:01:52 +0100

    correction

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config({ path: "sample.env" });
+
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MON, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
