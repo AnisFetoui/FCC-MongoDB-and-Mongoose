1a67bfa23294451ae8bb9ac814642fd8c5ea4289
Anis FETOUI <anis.fetoui@esprit.tn>
2023-12-26 02:15:24 +0100

    correction with add mongodb

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
-require('dotenv').config({path:'sample.env'});
+require('dotenv').config();
+const envs = process.env;
+const mongoose = require('mongoose');
+mongoose.connect(envs.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
